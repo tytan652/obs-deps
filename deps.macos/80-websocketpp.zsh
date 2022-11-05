78b230951b594f12aa2ef5aed5b3ab04f096f51e
tytan652 <tytan652@tytanium.xyz>
2022-11-05 16:55:07 +0100

    deps.macos: Add WebSocket++ to macOS deps

---
+++ deps.macos/80-websocketpp.zsh
@@ -0,0 +1,65 @@
+autoload -Uz log_debug log_error log_info log_status log_output
+
+## Dependency Information
+local name='websocketpp'
+local version='0.8.2'
+local url='https://github.com/zaphoyd/websocketpp.git'
+local hash='56123c87598f8b1dd471be83ca841ceae07f95ba'
+
+## Build Steps
+setup() {
+  log_info "Setup (%F{3}${target}%f)"
+  setup_dep ${url} ${hash}
+}
+
+clean() {
+  cd "${dir}"
+
+  if [[ ${clean_build} -gt 0 && -d "build_${arch}" ]] {
+    log_info "Clean build directory (%F{3}${target}%f)"
+
+    rm -rf "build_${arch}"
+  }
+}
+
+config() {
+  autoload -Uz mkcd progress
+
+  log_info "Config (%F{3}${target}%f)"
+
+  args=(
+    ${cmake_flags}
+    -DENABLE_CPP11=ON
+    -DBUILD_EXAMPLES=OFF
+    -DBUILD_TESTS=OFF
+  )
+
+  cd "${dir}"
+  log_debug "CMake configure options: ${args}"
+  progress cmake -S . -B "build_${arch}" -G Ninja ${args}
+}
+
+build() {
+  autoload -Uz mkcd
+
+  log_info "Build (%F{3}${target}%f)"
+
+  cd "${dir}"
+  cmake --build "build_${arch}" --config "${config}"
+}
+
+install() {
+  autoload -Uz progress
+
+  log_info "Install (%F{3}${target}%f)"
+
+  args=(
+    --install "build_${arch}"
+    --config "${config}"
+  )
+
+  if [[ "${config}" =~ "Release|MinSizeRel" ]] args+=(--strip)
+
+  cd "${dir}"
+  progress cmake ${args}
+}
