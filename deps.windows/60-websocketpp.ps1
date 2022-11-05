fc5da1da2e944b8575a4ba19a51fec8f108e2cc0
tytan652 <tytan652@tytanium.xyz>
2022-11-05 16:54:46 +0100

    deps.windows: Add WebSocket++ to Windows deps

---
+++ deps.windows/60-websocketpp.ps1
@@ -0,0 +1,65 @@
+param(
+    [string] $Name = 'websocketpp',
+    [string] $Version = '0.8.2',
+    [string] $Uri = 'https://github.com/zaphoyd/websocketpp.git',
+    [string] $Hash = '56123c87598f8b1dd471be83ca841ceae07f95ba'
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Clean {
+    Set-Location $Path
+
+    if ( Test-Path "build_${Target}" ) {
+        Log-Information "Clean build directory (${Target})"
+        Remove-Item -Path "build_${Target}" -Recurse -Force
+    }
+}
+
+function Configure {
+    Log-Information "Configure (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        $CmakeOptions
+        '-DENABLE_CPP11=ON'
+        '-DBUILD_EXAMPLES=OFF'
+        '-DBUILD_TESTS=OFF'
+    )
+
+    Invoke-External cmake -S . -B "build_${Target}" @Options
+}
+
+function Build {
+    Log-Information "Build (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--build', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $VerbosePreference -eq 'Continue' ) {
+        $Options += '--verbose'
+    }
+
+    Invoke-External cmake @Options
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--install', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $Configuration -match "(Release|MinSizeRel)" ) {
+        $Options += '--strip'
+    }
+
+    Invoke-External cmake @Options
+}
