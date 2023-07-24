00836f7f1bf37eca784c1d1c6d5b362bd4de8b5b
tytan652 <tytan652@tytanium.xyz>
2023-07-24 09:59:05 +0200

    deps.windows: Add uthash to Windows deps

---
+++ deps.windows/60-uthash.ps1
@@ -0,0 +1,56 @@
+param(
+    [string] $Name = 'uthash',
+    [string] $Version = '2.3.0',
+    [string] $Uri = 'https://github.com/troydhanson/uthash.git',
+    [string] $Hash = "e493aa90a2833b4655927598f169c31cfcdf7861"
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location "$Path"
+
+    New-Item -Path "$($ConfigData.OutputPath)/include" -ItemType Directory -Force *> $null
+
+    $Items = @(
+        @{
+            Path = "src/utarray.h"
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'    
+        }
+        @{
+            Path = "src/uthash.h"
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'    
+        }
+        @{
+            Path = "src/utlist.h"
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'    
+        }
+        @{
+            Path = "src/utringbuffer.h"
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'    
+        }
+        @{
+            Path = "src/utstack.h"
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'    
+        }
+        @{
+            Path = "src/utstring.h"
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'    
+        }
+    )
+
+    $Items | ForEach-Object {
+        $Item = $_
+        Log-Output ('{0} => {1}' -f ($Item.Path -join ", "), $Item.Destination)
+        Copy-Item @Item
+    }
+}
